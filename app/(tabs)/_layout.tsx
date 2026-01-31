0ac629513f04c56fde66f3552b370f8430cf0cf3
acs027 <alicihansarac@gmail.com>
2026-01-31 04:01:27 +0300

    Delete tab style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,13 +16,6 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
       }}>
       <Tabs.Screen
         name="index"
